2a57b64c09f28e43418935470abc2806506d0031
inakierregueab <inaki.erregue@gmail.com>
2023-04-17 16:19:51 +0200

    Add barrier to compensate rank0 doing more stuff

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -98,6 +98,9 @@ class BaseTrainer:
                 if epoch % self.save_period == 0:
                     self._save_checkpoint(epoch, save_best=best)
 
+            # TODO: makes sense? iteration time gets small improvement
+            torch.distributed.barrier()
+
     def _save_checkpoint(self, epoch, save_best=False):
         """
         Saving checkpoints

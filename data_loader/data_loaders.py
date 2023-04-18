bb3e26310cdd648a7abae3be027a8bc13295a2c9
inakierregueab <inaki.erregue@gmail.com>
2023-04-18 14:39:09 +0200

    Add repeated augmentation flag

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -21,6 +21,7 @@ class IMNETDataLoader(DataLoader):
                  is_distributed,
                  rank,
                  world_size,
+                 repeated_aug=False,
                  num_workers=0,
                  transform_config=None,
                  collate_fn=default_collate,
@@ -44,9 +45,12 @@ class IMNETDataLoader(DataLoader):
 
         if is_distributed:
             # TODO: test with RepeatedAugmentationSampler
-            self.train_sampler = RASampler(self.train_dataset, num_replicas=world_size, rank=rank, shuffle=True)
-            #self.train_sampler = DistributedSampler(self.train_dataset, num_replicas=world_size, rank=rank,
-            #                                        shuffle=True, drop_last=False)
+            if repeated_aug:
+                self.train_sampler = RASampler(self.train_dataset, num_replicas=world_size, rank=rank, shuffle=True)
+            else:
+                self.train_sampler = DistributedSampler(self.train_dataset, num_replicas=world_size, rank=rank,
+                                                        shuffle=True, drop_last=False)
+
             # TODO: add flag for distributed validation
             if len(self.val_dataset) % world_size != 0:
                 print('Warning: Enabling distributed evaluation with an eval dataset not divisible by process number. '
@@ -58,7 +62,7 @@ class IMNETDataLoader(DataLoader):
             #self.train_sampler = RandomSampler(self.train_dataset)
             #self.valid_sampler = SequentialSampler(self.val_dataset)
             self.train_sampler = RandomSampler(self.train_dataset, num_samples=300)
-            self.valid_sampler = RandomSampler(self.val_dataset, num_samples=300)
+            self.valid_sampler = RandomSampler(self.val_dataset, num_samples=100)
 
         # TODO: use persistent workers?
         self.init_kwargs = {

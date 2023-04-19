058238562290447a339d60c1254b22716560ca5d
inakierregueab <inaki.erregue@gmail.com>
2023-04-19 11:20:15 +0200

    Adjust search space

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import os
 import optuna
 
 import torch
+import torch.distributed as dist
 import numpy as np
 import data_loader.data_loaders as module_data
 import losses.loss as module_loss
@@ -25,27 +26,29 @@ def main(config, trials=None):
 
     # Hyperparameter optimization
     if trials is not None:
-        config['optimizer']['args']['lr'] = trials.suggest_float('lr', 1e-5, 1e-1)
-        config['data_loader']['args']['batch_size'] = trials.suggest_int('batch_size', 8, 64)
-        config['mixup']['args']['mixup_alpha'] = trials.suggest_float('alpha', 0.0, 1.0)
-        config['mixup']['args']['cutmix_alpha'] = trials.suggest_float('alpha', 0.0, 1.0)
+        config['optimizer']['args']['lr'] = trials.suggest_float('lr', 0.0001, 0.01)
+        config['optimizer']['args']['weight_decay'] = trials.suggest_float('weight_decay', 0.05, 0.5)
+        config['optimizer']['betas'] = [trials.suggest_float('beta1', 0.0, 1.0), trials.suggest_float('beta2', 0.0, 1.0)]
+
+        config['mixup']['args']['mixup_alpha'] = trials.suggest_float('alpha', 0.5, 1.0)
+        config['mixup']['args']['cutmix_alpha'] = trials.suggest_float('alpha', 0.5, 1.0)
         config['mixup']['args']['label_smoothing'] = trials.suggest_float('label_smoothing', 0.0, 0.5)
+
+        config['lr_scheduler']['warmup_epochs'] = trials.suggest_int('warmup_epochs', 3, 8)
 
 
     n_gpus = len(config['gpu_list'])
     is_distributed = n_gpus > 1
+
     if is_distributed:
         os.environ['MASTER_ADDR'] = 'localhost'
-        # watchmal automatically select port based on base gpu
         os.environ['MASTER_PORT'] = '12355'
 
         print("Using multiprocessing...")
         dev_ids = ["cuda:{0}".format(x) for x in config['gpu_list']]
         print("Using DistributedDataParallel on these devices: {}".format(dev_ids))
 
-        # TODO: fix printing, metrics and model state dict when ddp is used
         score = torch.multiprocessing.spawn(main_worker_function, nprocs=n_gpus, args=(n_gpus, is_distributed, config, trials))
-
     else:
         print("Not using multiprocessing...")
         score = main_worker_function(0, n_gpus, is_distributed, config, trials)
@@ -58,8 +61,7 @@ def main_worker_function(rank, world_size, is_distributed, config, trials):
     if is_distributed:
         device = config['gpu_list'][rank]
         print("Running main worker function on device: {}".format(device))
-        torch.distributed.init_process_group('nccl', init_method='env://', world_size=world_size, rank=rank)
-
+        dist.init_process_group('nccl', init_method='env://', world_size=world_size, rank=rank)
     else:
         device = prepare_device(config['gpu_list'])
 
@@ -80,7 +82,6 @@ def main_worker_function(rank, world_size, is_distributed, config, trials):
     # TODO: torch.compile() for faster inference HERE, are savable?
 
     # get function handles of loss and metrics
-    # TODO: criterion to device?
     criterion = config.init_obj('loss', module_loss, rank=rank)
     metrics = [getattr(module_metric, met) for met in config['metrics']]
 
@@ -102,7 +103,7 @@ def main_worker_function(rank, world_size, is_distributed, config, trials):
     score = trainer.train()
 
     if is_distributed:
-        torch.distributed.destroy_process_group()
+        dist.destroy_process_group()
 
     return score
 

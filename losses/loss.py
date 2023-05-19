d6112a34521553ab1e71fd1fbbbf1afd1607b4b9
inakierregueab <inaki.erregue@gmail.com>
2023-05-19 16:44:52 +0200

    Add new loss

--- losses/loss.py
+++ losses/loss.py
@@ -2,14 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from timm.loss import SoftTargetCrossEntropy
+from losses.softCE import SoftTargetCrossEntropy
 
 
 class DistillationLoss(nn.Module):
     def __init__(self, distillation_type='none', alpha=0, tau=1, rank=0):
         super().__init__()
         self.base_criterion = SoftTargetCrossEntropy()
-        assert distillation_type in ['none', 'soft_kl', 'soft_mse', 'hard']
+        assert distillation_type in ['none', 'soft_kl', 'soft_mse', 'soft_ce', 'hard']
         self.distillation_type = distillation_type
         if rank == 0:
             print(f'Distillation type is {distillation_type}')
@@ -45,6 +45,9 @@ class DistillationLoss(nn.Module):
 
         elif self.distillation_type == 'hard':
             distill_loss = F.cross_entropy(s_output, t_output.argmax(dim=1))
+
+        elif self.distillation_type == 'soft_ce':
+            distill_loss = SoftTargetCrossEntropy()(s_output, t_output, temperature=self.tau)
 
         loss = base_loss * (1-self.alpha) + distill_loss * self.alpha
         return loss, base_loss, distill_loss
